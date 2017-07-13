c8484d36134ce177047968ae975a970ac18033bf
Amit Upadhyay <amiit8@gmail.com>
2017-07-13 22:01:49 +0530

    created view to display fetched data

--- scripts/model.js
+++ scripts/model.js
@@ -4,18 +4,31 @@ var Model = function(config){
   }
   this.fetch = function(){
     var xhr = new XMLHttpRequest();
+    var self = this;
     xhr.onreadystatechange = function(){
       if(this.readyState  == 4 ){
         if(this.status == 200 || this.status == 304){
-          console.log(this.responseText);
+          self.handleSucess(this.responseText); 
         }
- 
+        else{
+          self.handleFailure();
+        }
+        
       }
     };
     xhr.open("GET", this.url, true);
     xhr.send();
   };
-
+  this.handleSucess = function(resp){
+    if(resp){
+      var parsedData = JSON.parse(resp);
+      this.data = parsedData;
+    }
+  };
+  this.handleFailure = function(resp){
+    this.data = null;
+    alert('Rest call failed');
+  };
 
 };
 
