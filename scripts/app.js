db93c71d7d7588762f3063a13dd2ff9102b6b4c8
Amit Upadhyay <amiit8@gmail.com>
2017-07-16 11:21:00 +0530

    added event listeners

--- scripts/app.js
+++ scripts/app.js
@@ -11,10 +11,37 @@ document.onreadystatechange = function(){
     mainView.model = posts;
     posts.view = mainView;
     mainView.render = function(){
-      var userIdEl = this.el.querySelector('.user-id');
+      var postIdEl = this.el.querySelector('.post-id');
+      var titleEl = this.el.querySelector('.title');  
+      var bodyEl = this.el.querySelector('.body');
+
       this.model.data.forEach(function(item){
-        userIdEl.options.add(new Option(item.userId, item.userId));
+        postIdEl.options.add(new Option(item.id, item.id));
       });
+
+      var self = this;
+      postIdEl.addEventListener('change', function(e){
+        var selectedId = parseInt(e.currentTarget.value);
+          var selectedPosts = self.model.data.filter(function(item){
+          return item.id === selectedId;
+        });
+        var selectedPost = selectedPosts.shift();
+
+        if(checkEven(selectedId)){
+          bodyEl.disabled = true;
+          bodyEl.innerHTML = selectedPost.body;
+          bodyEl.parentElement.parentElement.classList.remove('hidden');
+        }
+        else{
+          bodyEl.parentElement.parentElement.classList.add('hidden');
+        }
+
+        titleEl.options.add(new Option(selectedPost.title, selectedPost.title));
+        function checkEven(num){
+          return num%2 === 0;
+        }
+      });
+
       
     };
 
